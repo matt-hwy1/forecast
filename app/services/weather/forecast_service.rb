42dc03a6d614d205b7cb399ec33f85a978ad70a4
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-25 22:12:01 -0700

    Provide lookup method to encapsulate specific implementation

--- app/services/weather/forecast_service.rb
+++ app/services/weather/forecast_service.rb
@@ -4,6 +4,14 @@ module Weather
   class ForecastService
     class << self
       def forecast(location)
+        lookup_forecast(location)
+      end
+
+      private
+
+      # This method can be overriden to use a different API implementation.
+      # This can be useful for failover and redundancy
+      def lookup_forecast(location)
         Weather::VisualCrossing::Forecast.new(location).search(Weather::Clients::VisualCrossingClient.new)
       end
     end
