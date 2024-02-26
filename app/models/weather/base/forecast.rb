8ae1b99df1cc05c4051d03245492f3a5709211ed
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-25 22:02:04 -0700

    Add base class empty implementations

--- app/models/weather/base/forecast.rb
+++ app/models/weather/base/forecast.rb
@@ -8,6 +8,14 @@ module Weather
 
       validates :location, :original_location, :current_temp, :timestamp, :timezone,
           :humidity, :json, :tomorrow, :five_day, presence: true
+
+      def initialize(location)
+        raise NotImplementedError.new('Override this constructor with one that takes a String location')
+      end
+
+      def search(client)
+        raise NotImplementedError.new('Override this method and pass in an API client implementation of choice')
+      end
     end
   end
 end
--- app/models/weather/base/forecast_day.rb
+++ app/models/weather/base/forecast_day.rb
@@ -6,6 +6,10 @@ module Weather
       attr_accessor :high_temp, :low_temp, :temp, :description
 
       validates :high_temp, :low_temp, :temp, :description, presence: true
+
+      def initialize(json)
+        raise NotImplementedError.new('Override this constructor with one that takes a hash built from JSON data')
+      end
     end
   end
 end
