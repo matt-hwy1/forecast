c4c989eb091bca150549d29915c0475aa445ba27
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-25 19:05:55 -0700

    Update controller and request specs

---
+++ spec/requests/forecasts/forecasts_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Forecasts::Forecasts", type: :request do
+  describe "GET /" do
+    it "renders the search page" do
+      get root_path
+      expect(response).to render_template(:search)
+      expect(response.body).to include("Search")
+      expect(assigns[:forecast]).to be_nil
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET forecasts/search" do
+    let(:location) { '59937'}
+    let(:mock_forecast) do
+      forecast = FactoryBot.build(:forecast)
+    end
+
+    it "renders the search page" do
+      get forecasts_search_path
+      expect(response).to render_template(:search)
+      expect(response.body).to include("Search")
+      expect(assigns[:forecast]).to be_nil
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the api results if a location is provided" do
+      expect(Weather::ForecastService).to receive(:forecast).and_return(mock_forecast)
+
+      get forecasts_search_path(location: location)
+      expect(assigns[:forecast]).to_not be_nil
+      expect(response).to render_template(:search)
+      expect(response.body).to include("Weather forecast for")
+      expect(response).to have_http_status(200)
+    end
+  end
+end
