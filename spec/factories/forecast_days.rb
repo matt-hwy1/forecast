3cf7a87bbcc09faa188ac07605483250c8d819d0
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-26 09:08:59 -0700

    Update factories to return concrete subclass

--- spec/factories/forecast_days.rb
+++ spec/factories/forecast_days.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :forecast_day, class: 'weather/base/forecast_day' do
+  factory :forecast_day, class: 'weather/visual_crossing/forecast_day' do
     temp { 55 }
     low_temp { 45 }
     high_temp { 65 }
     description { "A nice sunny day" }
+
+    initialize_with { Weather::VisualCrossing::ForecastDay.new({}) }
   end
 end
--- spec/factories/forecasts.rb
+++ spec/factories/forecasts.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :forecast, class: 'weather/base/forecast' do
+  factory :forecast, class: 'weather/visual_crossing/forecast' do
     location { 'Kalispell, Montana' }
     original_location { '59901' }
     current_temp { '40' }
@@ -11,5 +11,7 @@ FactoryBot.define do
     tomorrow { build(:forecast_day) }
     five_day { build(:forecast_day) }
     error { nil }
+
+    initialize_with { Weather::VisualCrossing::Forecast.new('A location') }
   end
 end
