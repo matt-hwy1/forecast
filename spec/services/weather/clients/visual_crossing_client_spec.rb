df9557ee8f6a115381b17425ebffeb28364fd560
Matt Schwartz <matt.schwartz@verafi.net>
2024-02-25 21:05:00 -0700

    Add spec for API client

---
+++ spec/services/weather/clients/visual_crossing_client_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Weather::Clients::VisualCrossingClient do
+  it "calls the external VisualCrossing API" do
+    expect(Faraday).to receive(:get).and_return(double('response', status: 200, body: '{}'))
+    subject.call("some location")
+  end
+end
